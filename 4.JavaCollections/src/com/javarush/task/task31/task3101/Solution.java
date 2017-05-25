ee84c6fd67636c206bb257aba7acef037229e57f
Ivan Blinov <iv.blinovv@gmail.com>
2017-05-25 20:32:51 +0300

    Collections tasks added

--- 4.JavaCollections/src/com/javarush/task/task31/task3101/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3101/Solution.java
@@ -1,70 +1,88 @@
 package com.javarush.task.task31.task3101;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.LinkedList;
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
 
-/*
-Проход по дереву файлов
+/* Проход по дереву файлов
+1. На вход метода main подаются два параметра.
+Первый - path - путь к директории, второй - resultFileAbsolutePath - имя файла, который будет содержать результат.
+2. Для каждого файла в директории path и в ее всех вложенных поддиректориях выполнить следующее:
+2.1. Если у файла длина в байтах больше 50, то удалить его.
+2.2. Если у файла длина в байтах НЕ больше 50, то для всех таких файлов:
+2.2.1. отсортировать их по имени файла в возрастающем порядке, путь не учитывать при сортировке
+2.2.2. переименовать resultFileAbsolutePath в 'allFilesContent.txt'
+2.2.3. в allFilesContent.txt последовательно записать содержимое всех файлов из п. 2.2.1. Тела файлов разделять "\n"
+2.3. Удалить директории без файлов (пустые).
+Все файлы имеют расширение txt.
 */
 public class Solution {
     public static void main(String[] args) throws IOException {
-        File folder = new File(args[0]);
-        File result = new File(args[1]);
+        String directoryPath = args[0];
+        String resultFileAbsolutePath = args[1];
+        //String directoryPath = "src/main/resources/level31/lesson02/home01";
+        //String resultFileAbsolutePath = "src/main/resources/level31/lesson02/home01/res.txt";
+        String allFilesContent = "allFilesContent.txt";
 
-        String newName = result.getParent() + "/allFilesContent.txt";
-        FileUtils.renameFile(result, new File(newName));
+        saveFilesLessThan50Bytes(directoryPath);
 
-        ArrayList<File> files = new ArrayList<>();
-        getFiles(folder,files);
+        // rename file
+        Path source = Paths.get(resultFileAbsolutePath);
+        Path newResPath = Files.move(source, source.resolveSibling(allFilesContent));
 
-        for (File file : files) {
-            if (file.length() > 50) {
-                FileUtils.deleteFile(file);
-                files.remove(file);
-            }
-        }
+        // delete resultFileAbsolutePath if list contains it
+        lessThan50BytesFiles.remove(new File(resultFileAbsolutePath));
 
-        Collections.sort(files, new Comparator<File>() {
+        // sort file by name
+        Collections.sort(lessThan50BytesFiles, new Comparator<File>() {
             @Override
             public int compare(File o1, File o2) {
-                return o1.getName().compareTo(o2.getName());
+                String fileName1 = o1.getName();
+                String fileName2 = o2.getName();
+                return fileName1.compareTo(fileName2);
             }
         });
 
-        FileInputStream fileInputStream = null;
-        FileOutputStream fileOutputStream = new FileOutputStream(result);
-        for (File file : files) {
-            fileInputStream = new FileInputStream(file);
-            byte[] buffer = new byte[fileInputStream.available()];
-            fileInputStream.read(buffer);
-            fileOutputStream.write(buffer);
+        // write to result file
+        BufferedWriter fout = new BufferedWriter(new FileWriter(newResPath.toFile()));
+        for (File file : lessThan50BytesFiles) {
+            BufferedReader fin = new BufferedReader(new FileReader(file));
+            while (fin.ready()) {
+                fout.write(fin.readLine());
+                fout.newLine();
+            }
+            fin.close();
         }
-        fileOutputStream.close();
-        if (fileInputStream != null) fileInputStream.close();
-
+        fout.close();
     }
 
-    public static void getFiles (File directory, ArrayList<File> files) {
-        for (File file : directory.listFiles()) {
-            if (file.isDirectory()) {
-                if (file.length() == 0) {
-                    file.delete();
+    private static List<File> lessThan50BytesFiles = new ArrayList<>();
+
+    private static void saveFilesLessThan50Bytes(String directory) {
+        File dir = new File(directory);
+        File[] files = dir.listFiles();
+
+        if (files == null) {
+            // it is not directory
+            return;
+        } else if (files.length == 0) {
+            // directory is empty
+            dir.delete();
+        } else {
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    // nested directory
+                    saveFilesLessThan50Bytes(file.getAbsolutePath());
                 } else {
-                    getFiles(file,files);
+                    if (file.length() > 50) {
+                        file.delete();
+                    } else {
+                        lessThan50BytesFiles.add(file);
+                    }
                 }
-            } else {
-                files.add(file);
             }
         }
-    }
-
-    public static void deleteFile(File file) {
-        if (!file.delete()) System.out.println("Can not delete file with name " + file.getName());
     }
 }
--- 4.JavaCollections/src/com/javarush/task/task31/task3105/Solution.java
+++ 4.JavaCollections/src/com/javarush/task/task31/task3105/Solution.java
@@ -1,6 +1,7 @@
 package com.javarush.task.task31.task3105;
 
 import java.io.*;
+
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.HashMap;
@@ -9,9 +10,31 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
-/* 
-Добавление файла в архив
+
+/* Добавление файла в архив
+В метод main приходит список аргументов.
+Первый аргумент - полный путь к файлу fileName.
+Второй аргумент - путь к zip-архиву.
+Добавить файл (fileName) внутрь архива в директорию 'new'.
+Если в архиве есть файл с таким именем, то заменить его.
+Пример входных данных:
+C:/result.mp3
+C:/pathToTest/test.zip
+Файлы внутри test.zip:
+a.txt
+b.txt
+После запуска Solution.main архив test.zip должен иметь такое содержимое:
+new/result.mp3
+a.txt
+b.txt
+Подсказка: нужно сначала куда-то сохранить содержимое всех энтри,
+а потом записать в архив все энтри вместе с добавленным файлом.
+Пользоваться файловой системой нельзя.
 */
+
+// !!!! Закидывать файл в архив нужно только если он там уже есть, иначе не принимается.
+
+
 public class Solution {
 
     public static Map <ZipEntry, byte[]> entryMap = new HashMap<>();
